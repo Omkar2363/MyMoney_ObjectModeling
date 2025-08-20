b81e07314d25c15825628d1da85635ff18306cec
Omkar2363 <om.236300@gmail.com>
2025-08-20 20:32:30 +0530

    Entity Constants (magic numbers solution)

---
+++ src/main/java/com/geektrust/backend/constants/EntityConstants.java
@@ -0,0 +1,14 @@
+package com.geektrust.backend.constants;
+
+public class EntityConstants {
+    // Constants to remove magic numbers
+    public static final double MINIMUM_ALLOCATION_RATIO = 0.0;
+    public static final int MINIMUM_AMOUNT = 0;
+    public static final double ROI_MULTIPLIER_BASE = 1.0;
+    public static final double MINIMUM_FINAL_AMOUNT = 0.0;
+
+    public static final double DEFAULT_ROI = 0.0;
+
+    private EntityConstants() {}
+
+}
