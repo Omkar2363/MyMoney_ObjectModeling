cb6dccea52926b299bab1cd8fa6b6cef00de3dde
Omkar2363 <om.236300@gmail.com>
2025-08-20 20:37:41 +0530

    Portfolio service (magic number solution)

---
+++ src/main/java/com/geektrust/backend/constants/ServiceConstants.java
@@ -0,0 +1,12 @@
+package com.geektrust.backend.constants;
+
+public class ServiceConstants {
+    // Constants to remove magic numbers
+    public static final int MINIMUM_TOTAL_ALLOCATION = 0;
+    public static final int DEFAULT_ALLOCATION_AMOUNT = 0;
+    public static final boolean INITIAL_SIP_STATE = false;
+
+    private ServiceConstants() {}
+}
+
+
